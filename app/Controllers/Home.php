8748e4665084a77f911413bf542a92e87e199dd9
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-04-12 19:44:13 +0700

    Fix up visitor template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('beranda');
+        return view('visitor/beranda');
     }
 }
--- app/Views/layout/visitor_template.php
+++ app/Views/layout/visitor_template.php
@@ -17,145 +17,9 @@
 </head>
 <body>
 
-<script type='text/javascript'>
-	var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
-	var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
-	var date = new Date();
-	var day = date.getDate();
-	var month = date.getMonth();
-	var thisDay = date.getDay(),
-	    thisDay = myDays[thisDay];
-	var yy = date.getYear();
-	var year = (yy < 1000) ? yy + 1900 : yy;
-	
-	function startTime() {
-		var today=new Date(),
-		curr_hour=today.getHours(),
-		curr_min=today.getMinutes(),
-		curr_sec=today.getSeconds();
-		curr_hour=checkTime(curr_hour);
-		curr_min=checkTime(curr_min);
-		curr_sec=checkTime(curr_sec);
-		document.getElementById('clock').innerHTML=thisDay + ', ' + day + ' ' + months[month] + ' ' + year + '  ' + curr_hour+":"+curr_min+":"+curr_sec;
-	}
-	function checkTime(i) {
-		if (i<10) {
-			i="0" + i;
-		}
-		return i;
-	}
-	setInterval(startTime, 500);
-</script>
-
 <div class="super_container">
 	<!-- Header -->
 	<header class="header">
-		<div class="hidden-xs hidden-sm">
-			<div style="background-color: #FFFFFF;">
-				<div class="container">
-					<div class="row">
-						<div class="col-sm-4" style="color:#000; text-align:left; padding-left:55px">
-							<div style="margin-top:8px"><i class="fa fa-clock-o" style="color: #000000"></i>
-								<span id="clock"></span>
-							</div>			
-						</div>
-						<div class="col-md-1" style="padding: 0px;margin:0px;">
-						</div>
-						<div class="col-md-5" style="padding-right:0px; ">
-							<div class="">
-								<div class="row" style="margin:unset">
-									<div class="col-sm-12" style="background-color: #ffffff">
-										<!-- get marquee css -->
-										<script type="text/javascript">
-											var use_debug = false;
-											function debug(){
-												if( use_debug && window.console && window.console.log ) console.log(arguments);
-											}
-											// on DOM ready
-											$(document).ready(function (){
-												$(".marquee").marquee({
-													loop: -1
-													// this callback runs when the marquee is initialized
-													, init: function ($marquee, options){
-														debug("init", arguments);
-														// shows how we can change the options at runtime
-														if( $marquee.is("#marquee2") ) options.yScroll = "bottom";
-													}
-													// this callback runs before a marquee is shown
-													, beforeshow: function ($marquee, $li){
-														debug("beforeshow", arguments);
-														// check to see if we have an author in the message (used in #marquee6)
-														var $author = $li.find(".author");
-														// move author from the item marquee-author layer and then fade it in
-														if( $author.length ){
-															$("#marquee-author").html("<span style='display:none;'>" + $author.html() + "</span>").find("> span").fadeIn(850);
-														}
-													}
-													// this callback runs when a has fully scrolled into view (from either top or bottom)
-													, show: function (){
-														debug("show", arguments);
-													}
-													// this callback runs when a after message has being shown
-													, aftershow: function ($marquee, $li){
-														debug("aftershow", arguments);
-														// find the author
-														var $author = $li.find(".author");
-														// hide the author
-														if( $author.length ) $("#marquee-author").find("> span").fadeOut(250);
-													}
-												});
-											});
-															
-											var iNewMessageCount = 0;
-											
-											function addMessage(selector){
-												// increase counter
-												iNewMessageCount++;
-												// append a new message to the marquee scrolling list
-												var $ul = $(selector).append("<li>New message #" + iNewMessageCount + "</li>");
-												// update the marquee
-												$ul.marquee("update");
-											}
-															
-											function pause(selector){
-												$(selector).marquee('pause');
-											}
-											
-											function resume(selector){
-												$(selector).marquee('resume');
-											}
-										</script>
-										<!-- ul running text -->
-										<!-- <ul id="marquee1" class="marquee" style="margin-top:5px;">
-											<li style="color:#000000">Selamat Datang di Website Resmi Kejaksaan Negeri Denpasar. Kejaksaan Negeri Denpasar Solid Menuju Wilayah Birokrasi Bersih Melayani</li>
-										</ul> -->
-
-									</div>
-								</div>
-							</div>
-						</div><!-- .col-md-offset-2 col-md-6 -->
-
-						<div class="col-md-2" style="padding: 7px; text-align:left">
-							<span class="margin-right-5 margin-top-10">
-								<a target="_blank" href="https://www.instagram.com/kejari_denpasar/"style="">
-									<b><i style="font-size:20px; width:30px; color:#000" class="fa fa-instagram"></i></b>
-								</a>    
-							</span>
-							<span class="margin-right-5 margin-top-10">	
-								<a target="_blank" href="https://www.youtube.com/kejaridenpasar"style="">
-									<b><i style="font-size:20px; width:30px; color:#000" class="fa fa-youtube"></i></b>
-								</a>    
-							</span>
-							<span class="margin-right-5 margin-top-10">
-								<a target="_blank" href="https://www.facebook.com/kejaridenpasar"style="">
-									<b><i style="font-size:20px; width:30px; color:#000" class="fa fa-facebook"></i></b>
-								</a>    
-							</span>
-						</div> <!-- .col-md-4 -->
-					</div> <!-- .row -->
-        </div> <!-- .container -->
-    	</div>
-		</div>
 		<div class="container">
 			<div class="row">
 				<div class="col">
@@ -276,11 +140,11 @@
 						<!-- Advertising 2 -->
 						<br>
 						<div class="sidebar_section">
-							<a href=""><img src="<?=base_url()?>/template/visitor/images/lapor.jpg" alt="" width="100%"></a>
+							<a href="https://www.lapor.go.id/"><img src="<?=base_url()?>/template/visitor/images/lapor.jpg" alt="" width="100%"></a>
 						</div>
 						<br>
 						<div class="sidebar_section">
-							<a href=""><img src="<?=base_url()?>/template/visitor/images/laporkan.jpg" alt="" width="100%"></a>
+							<a href="https://www.kejaksaan.go.id/pengaduan.php"><img src="<?=base_url()?>/template/visitor/images/laporkan.jpg" alt="" width="100%"></a>
 						</div>
 
 
@@ -350,10 +214,8 @@
 								</a>
 							</div>
 						</div>
-
 					</div>
 				</div>
-
 			</div>
 		</div>
 	</div>
@@ -363,39 +225,44 @@
 	<footer class="footer">
 		<div class="container">
 			<div class="row row-lg-eq-height">
-				<div class="col-lg-9 order-lg-1 order-2">
+				<div class="col-3">
 					<div class="footer_content">
-						<div class="footer_logo"><a href="#">avision</a></div>
-						<div class="footer_social">
-							<ul>
-								<li class="footer_social_facebook"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-								<li class="footer_social_twitter"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-								<li class="footer_social_pinterest"><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-								<li class="footer_social_vimeo"><a href="#"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
-								<li class="footer_social_instagram"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-								<li class="footer_social_google"><a href="#"><i class="fa fa-google" aria-hidden="true"></i></a></li>
-							</ul>
-						</div>
-						<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>. Downloaded from <a href="https://themeslab.org/" target="_blank">Themeslab</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+						<p style="color: white; text-align: left;"><b>Kontak Informasi</b></p>
+						<p style="color: white; text-align: left;">Kejaksaan Negeri Purwokerto</p>
+						<p style="color: white; text-align: left;"><i class="fa fa-map-marker"></i> Jl. Gatot Subroto No.109 Purwokerto</p>
+						<p style="color: white; text-align: left;"><i class="fa fa-envelope"></i> info@kejari-purwokerto.go.id</p>
+						<p style="color: white; text-align: left;"><i class="fa fa-globe"></i> http://kejari-purwokerto.go.id</p>
+						<p style="color: white; text-align: left;"><i class="fa fa-phone"></i> (0281) 635590</p>
 					</div>
 				</div>
-				<div class="col-lg-3 order-lg-2 order-1">
-					<div class="subscribe">
-						<div class="subscribe_background"></div>
-						<div class="subscribe_content">
-							<div class="subscribe_title">Subscribe</div>
-							<form action="#">
-								<input type="email" class="sub_input" placeholder="Your Email" required="required">
-								<button class="sub_button">
-									<svg version="1.1" id="link_arrow_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-										 width="19px" height="13px" viewBox="0 0 19 13" enable-background="new 0 0 19 13" xml:space="preserve">
-										<polygon fill="#FFFFFF" points="12.475,0 11.061,0 17.081,6.021 0,6.021 0,7.021 17.038,7.021 11.06,13 12.474,13 18.974,6.5 "/>
-									</svg>
-								</button>
-							</form>
-						</div>
+				<div class="col-3">
+					<div class="footer_content">
+						<p style="color: white; text-align: left;"><b>Statistik</b></p>
+						<p style="color: white; text-align: left;">User Online : 3</p>
+						<p style="color: white; text-align: left;">Today Visitor : 527</p>
+						<p style="color: white; text-align: left;">Hits hari ini : 2189</p>
+						<p style="color: white; text-align: left;">Total Pengunjung : 31300</p>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="footer_content">
+						<p style="color: white; text-align: left;"><b>Situs Terkait</b></p>
+						<p style="color: white; text-align: left;"><a style="color: white;" href="https://www.kejaksaan.go.id/"><i class="fa fa-globe"></i> Kejaksaan RI</a></p>
+						<p style="color: white; text-align: left;"><a style="color: white;" href="https://www.pn-purwokerto.go.id/"><i class="fa fa-globe"></i> Pengadilan Negeri Purwokerto</a></p>
+						<p style="color: white; text-align: left;"><a style="color: white;" href="https://www.mahkamahagung.go.id/"><i class="fa fa-globe"></i> Mahkamah Agung</a></p>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="footer_content">
+						<p style="color: white; text-align: left;"><b>Sosial Media</b></p>
+
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<div class="copyright align-items-center justify-content-center" style="color: #FFFFFF;">
+						Copyright &copy;<script>document.write(new Date().getFullYear());</script> Kejaksaan Negeri Purwokerto. All rights reserved
 					</div>
 				</div>
 			</div>
