28ed7882718e3bb685edd963d1c7e56ba1344468
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-04-14 04:48:06 +0700

    Pidum

--- app/Views/visitor/bidang/pidum.php
+++ app/Views/visitor/bidang/pidum.php
@@ -11,7 +11,12 @@
               <div class="section_title">Pidana Umum</div>
             </div>
             <div class="section_content">
-              
+              <div style="text-align:center">
+                <img src="<?= base_url() ?>/template/visitor/images/kepala.jpg" alt="" width="300px">
+                <p>Nama</p>
+                <p>NIP</p>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit deleniti, blanditiis consectetur nihil natus modi nesciunt inventore fuga numquam esse laudantium quia animi exercitationem expedita incidunt eaque eos omnis accusantium.</p>
             </div>
           </div>
         </div>
