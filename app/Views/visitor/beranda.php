fd7523378eb4735abafccb82b34ad34fdcd21ca5
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-04-18 20:07:58 +0700

    Foto Galeri

--- app/Views/visitor/beranda.php
+++ app/Views/visitor/beranda.php
@@ -76,7 +76,7 @@
 									<!-- Small Card With Background -->
 									<?php $i = 0;
 									foreach ($pelayanan as $data) :  ?>
-										<a href="<?= $data['url_pelayanan']; ?>">
+										<a href="<?= $data['url_pelayanan']; ?>" target="_blank">
 											<div class="card card_default card_small_with_background grid-item md-sm">
 												<div class="card_background" style="background-image: linear-gradient(45deg, <?= $data['warna_pelayanan']; ?>, <?= $data['gradiasi_pelayanan']; ?>)"></div>
 												<div class="card-body">
@@ -410,6 +410,70 @@
 								<div class="section_title">Foto Galeri</div>
 							</div>
 							<div class="section_content">
+
+								<div class="container">
+									<div class="row">
+										<div class="MultiCarousel" data-items="1,2,3,3" data-slide="1" id="MultiCarousel" data-interval="1000">
+											<div class="MultiCarousel-inner">
+												<div class="item">
+													<div class="pad15">
+														<img src="<?= base_url() ?>/template/visitor/images/post_6.jpg" alt="" width="100%">
+														<p><i class="fa fa-clock-o"></i> 25 April 2022</p>
+														<p style="font-weight: bold;">Selamat Hari Raya Nyepi Tahun Baru Saka 1944</p>
+													</div>
+												</div>
+												<div class="item">
+													<div class="pad15">
+														<img src="<?= base_url() ?>/template/visitor/images/post_6.jpg" alt="" width="100%">
+														<p><i class="fa fa-clock-o"></i> 25 April 2022</p>
+														<p style="font-weight: bold;">Selamat Hari Raya Nyepi Tahun Baru Saka 1944</p>
+													</div>
+												</div>
+												<div class="item">
+													<div class="pad15">
+														<img src="<?= base_url() ?>/template/visitor/images/post_6.jpg" alt="" width="100%">
+														<p><i class="fa fa-clock-o"></i> 25 April 2022</p>
+														<p style="font-weight: bold;">Selamat Hari Raya Nyepi Tahun Baru Saka 1944</p>
+													</div>
+												</div>
+												<div class="item">
+													<div class="pad15">
+														<img src="<?= base_url() ?>/template/visitor/images/post_6.jpg" alt="" width="100%">
+														<p><i class="fa fa-clock-o"></i> 25 April 2022</p>
+														<p style="font-weight: bold;">Selamat Hari Raya Nyepi Tahun Baru Saka 1944</p>
+													</div>
+												</div>
+												<div class="item">
+													<div class="pad15">
+														<img src="<?= base_url() ?>/template/visitor/images/post_6.jpg" alt="" width="100%">
+														<p><i class="fa fa-clock-o"></i> 25 April 2022</p>
+														<p style="font-weight: bold;">Selamat Hari Raya Nyepi Tahun Baru Saka 1944</p>
+													</div>
+												</div>
+												<div class="item">
+													<div class="pad15">
+														<img src="<?= base_url() ?>/template/visitor/images/post_6.jpg" alt="" width="100%">
+														<p><i class="fa fa-clock-o"></i> 25 April 2022</p>
+														<p style="font-weight: bold;">Selamat Hari Raya Nyepi Tahun Baru Saka 1944</p>
+													</div>
+												</div>
+
+											</div>
+											<button class="carousel-control-prev-icon leftLst"></button>
+											<button class="carousel-control-next-icon rightLst"></button>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12 text-center">
+											<br /><br /><br />
+											<hr />
+											<p>Settings</p>
+											<p>Change data items for xs,sm,md and lg display items respectively. Ex:data-items="1,3,5,6"</p>
+											<p>Change data slide for slides per click Ex:data-slide="1"</p>
+										</div>
+									</div>
+								</div>
+
 								<div class="grid clearfix">
 
 									<!-- Small Card With Image -->
@@ -559,6 +623,108 @@
 							$('#p_perdata').show();
 							$('#p_khusus').hide();
 						});
+					});
+
+					$(document).ready(function() {
+						var itemsMainDiv = ('.MultiCarousel');
+						var itemsDiv = ('.MultiCarousel-inner');
+						var itemWidth = "";
+
+						$('.leftLst, .rightLst').click(function() {
+							var condition = $(this).hasClass("leftLst");
+							if (condition)
+								click(0, this);
+							else
+								click(1, this)
+						});
+
+						ResCarouselSize();
+						$(window).resize(function() {
+							ResCarouselSize();
+						});
+
+						//this function define the size of the items
+						function ResCarouselSize() {
+							var incno = 0;
+							var dataItems = ("data-items");
+							var itemClass = ('.item');
+							var id = 0;
+							var btnParentSb = '';
+							var itemsSplit = '';
+							var sampwidth = $(itemsMainDiv).width();
+							var bodyWidth = $('body').width();
+							$(itemsDiv).each(function() {
+								id = id + 1;
+								var itemNumbers = $(this).find(itemClass).length;
+								btnParentSb = $(this).parent().attr(dataItems);
+								itemsSplit = btnParentSb.split(',');
+								$(this).parent().attr("id", "MultiCarousel" + id);
+
+
+								if (bodyWidth >= 1200) {
+									incno = itemsSplit[3];
+									itemWidth = sampwidth / incno;
+								} else if (bodyWidth >= 992) {
+									incno = itemsSplit[2];
+									itemWidth = sampwidth / incno;
+								} else if (bodyWidth >= 768) {
+									incno = itemsSplit[1];
+									itemWidth = sampwidth / incno;
+								} else {
+									incno = itemsSplit[0];
+									itemWidth = sampwidth / incno;
+								}
+								$(this).css({
+									'transform': 'translateX(0px)',
+									'width': itemWidth * itemNumbers
+								});
+								$(this).find(itemClass).each(function() {
+									$(this).outerWidth(itemWidth);
+								});
+
+								$(".leftLst").addClass("over");
+								$(".rightLst").removeClass("over");
+
+							});
+						}
+
+
+						//this function used to move the items
+						function ResCarousel(e, el, s) {
+							var leftBtn = ('.leftLst');
+							var rightBtn = ('.rightLst');
+							var translateXval = '';
+							var divStyle = $(el + ' ' + itemsDiv).css('transform');
+							var values = divStyle.match(/-?[\d\.]+/g);
+							var xds = Math.abs(values[4]);
+							if (e == 0) {
+								translateXval = parseInt(xds) - parseInt(itemWidth * s);
+								$(el + ' ' + rightBtn).removeClass("over");
+
+								if (translateXval <= itemWidth / 2) {
+									translateXval = 0;
+									$(el + ' ' + leftBtn).addClass("over");
+								}
+							} else if (e == 1) {
+								var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+								translateXval = parseInt(xds) + parseInt(itemWidth * s);
+								$(el + ' ' + leftBtn).removeClass("over");
+
+								if (translateXval >= itemsCondition - itemWidth / 2) {
+									translateXval = itemsCondition;
+									$(el + ' ' + rightBtn).addClass("over");
+								}
+							}
+							$(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+						}
+
+						//It is used to get some elements from btn
+						function click(ell, ee) {
+							var Parent = "#" + $(ee).parent().attr("id");
+							var slide = $(Parent).attr("data-slide");
+							ResCarousel(ell, Parent, slide);
+						}
+
 					});
 				</script>
 				<?= $this->endSection(); ?>
